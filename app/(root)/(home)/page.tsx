9e90f9eebc918701ec12158969346af701de7d2f
Mimo <mimoriam420@gmail.com>
2023-10-26 15:52:25 +0500

    Commit #5 - Clerk authentication

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <main>
-      <div>Hello!</div>
+      <div>
+        <UserButton afterSignOutUrl="/" />
+      </div>
     </main>
   );
 }
